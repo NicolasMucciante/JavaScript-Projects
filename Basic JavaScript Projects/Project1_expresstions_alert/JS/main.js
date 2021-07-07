60b5f8909524426a138ead3b2a96d9f3fed99301
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-07-06 20:36:10 -0700

    Basic JavaScript

---
+++ Basic JavaScript Projects/Project1_expresstions_alert/JS/main.js
@@ -0,0 +1,17 @@
+A = "Hello" // assigning hello to the var "A"
+window.alert(A); // pop up window that says hello
+document.write ("\"I am a winner,"
++ " I am the winner.\"") // printed text on the page
+var B = " Dogs and cats are awesome!" + " Goldfish are cool too."
+document.write(B)
+var Family = " The Mucciantes", Dad = " Tim", Mom = " Cheri", Daughter = " Kaylie", Son = " Nick"; // created the variable "Family" and added members
+document.write(Family); 
+var Dad = Dad.fontcolor("blue"); // makes the "Dad" text blue
+document.write(Dad);
+var Mom = Mom.fontcolor("Green"); // makes the "Mom" text green
+document.write(Mom);
+var Daughter = Daughter.fontcolor("red"); // makes the "Daughter" text red
+document.write(Daughter);
+var Son = Son.fontcolor("Orange"); // makes the "Son" text orange
+document.write(Son);
+document.write(5+5); 
